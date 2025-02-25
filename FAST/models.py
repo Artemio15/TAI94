853f0148b6433ef7793307acd318c6e81ea1f29d
Artemio Anaya Martínez <anayamartinezartemio@gmail.com>
2025-02-24 18:45:06 -0600

    ValidacionFin

--- FAST/models.py
+++ FAST/models.py
@@ -4,6 +4,4 @@ class modelUsuario(BaseModel):
     id:int = Field(...,gt=0, description="Id siempre debe ser positivo")
     nombre:str = Field(...,min_length=1, max_length=85 ,description=" solo letras y espacios min 1 max 85")
     edad:int = Field(..., gt=0, ge=120, description="Edad debe estar entre 0 y 120 años")
-    correo: str = Field(..., regex=r"^.@.com$", description="Debe ser un correo válido")
-    
-
+    correo: str = Field(..., pattern=r"^.@.com$", example="usuario@example.com", description="Correo válido") 
